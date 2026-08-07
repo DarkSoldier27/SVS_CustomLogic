2df35e38d44e3db36c2524b0863cfa3ac9e50f7d
DarkSoldier27 <Darksoldier27@gmail.com>
2026-08-07 03:35:05 -0400

    Add files via upload

---
+++ SVS_CustomLogic/SVS_CustomLogic/CustomLogicParam.cs
@@ -0,0 +1,25 @@
+﻿namespace SVS_CustomTraits
+{
+    internal class CustomLogicParam
+    {
+        public class TraitParam
+        {
+            public string TraitName { get; set; }
+            public int TraitID { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class LewdTraitParam
+        {
+            public string LewdTraitName { get; set; }
+            public int LewdTraitID { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class JobParam
+        {
+            public string JobName { get; set; }
+            public int JobID { get; set; }
+        }
+    }
+}
