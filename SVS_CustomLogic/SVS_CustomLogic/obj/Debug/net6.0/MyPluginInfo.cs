da08e5e54b8d7e443c456e6c7bd87d773f2ec6e0
DarkSoldier27 <Darksoldier27@gmail.com>
2026-08-07 03:39:32 -0400

    Delete SVS_CustomLogic/SVS_CustomLogic directory

--- SVS_CustomLogic/SVS_CustomLogic/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SVS_CustomTraits
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SVS_CustomTraits";
-        public const string PLUGIN_NAME = "Custom traits for SVS";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
